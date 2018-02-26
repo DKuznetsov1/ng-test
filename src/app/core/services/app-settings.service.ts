986c6bbe9638e6c4bf1114358c6bdc8065c778af
DKuznetsov1 <Dmytro_Kuznetsov@epam.com>
2018-02-26 13:09:53 +0200

    Task6: tmp fix for getting settings

--- src/app/core/services/app-settings.service.ts
+++ src/app/core/services/app-settings.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 // rxjs
 import { toPromise } from 'rxjs/operator/toPromise';
@@ -21,14 +21,19 @@ export class AppSettingsService {
     const existingConfig = JSON.parse(this.localStorageService.getItem(this.configKey));
 
     if (!existingConfig) {
-      this.http.get('assets/app-settings.json')
-        .toPromise()
-        .then((configFromFile) => {
-          const config = configFromFile || new Config('localhost');
-          console.log('Got config from file or default');
-          this.saveConfig(config);
-          return config;
-        });
+      return Promise.resolve(new Config('http://localhost:3000/'));
+      // this.http.get('assets/app-settings.json')
+      //   .toPromise()
+      //   .then((configFromFile) => {
+      //     const config = configFromFile || new Config('http://localhost:3000/');
+      //     console.log('Got config from file or default');
+      //     this.saveConfig(config);
+      //     return config;
+      //   })
+      //   .catch(err => {
+      //     console.log(err.message);
+      //     throw err;
+      //   });
     } else {
       console.log('Got existing config');
       return Promise.resolve(existingConfig);
